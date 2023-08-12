1c28f7823a25b43d0e8a3d4ced2584e891d6f1f2
barit <mmyers.com@Gmail.com>
2023-08-12 00:34:11 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const ul = document.getElementsByTagName("main")[0];
+ul.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.textContent = "Michael is the champion";
+newHeader.setAttribute("id", "victory");
+document.head.appendChild(newHeader);
